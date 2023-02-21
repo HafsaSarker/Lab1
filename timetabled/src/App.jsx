b1bdb9c8158546c1e132e0d42f2edd3d05e05126
Zaara <hafsahaqs@gmail.com>
2023-02-21 17:30:22 -0500

    add header

--- timetabled/src/App.jsx
+++ timetabled/src/App.jsx
@@ -4,7 +4,7 @@ const App = () => {
 
   return (
     <div className="App">
-      <h1>Itinerary for 7 Days in Japan</h1>
+      <h1>Itinerary for 7 Days in Japan 🌃</h1>
       <h2>One week of amazing food, beautiful cherry blossoms and historic temples (not to mention introvert friendly restaurants)</h2>
     </div>
   )
