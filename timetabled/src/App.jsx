9b285ac460df0faca08296cc4bd23f63953d7f74
Zaara <hafsahaqs@gmail.com>
2023-02-21 17:18:11 -0500

    add files

---
+++ timetabled/src/App.jsx
@@ -0,0 +1,13 @@
+import './App.css';
+
+const App = () => {
+
+  return (
+    <div className="App">
+      <h1>Itinerary for 7 Days in Japan</h1>
+      <h2>One week of amazing food, beautiful cherry blossoms and historic temples (not to mention introvert friendly restaurants)</h2>
+    </div>
+  )
+}
+
+export default App
