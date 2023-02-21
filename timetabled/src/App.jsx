a524945bbf89f8d461de6bfb62a45ed520f6d9c7
Zaara <hafsahaqs@gmail.com>
2023-02-21 17:55:01 -0500

    add calendar

--- timetabled/src/App.jsx
+++ timetabled/src/App.jsx
@@ -1,11 +1,12 @@
 import './App.css';
-
+import Calendar from './components/Calendar';
 const App = () => {
 
   return (
     <div className="App">
       <h1>Itinerary for 7 Days in Japan 🌃</h1>
       <h2>One week of amazing food, beautiful cherry blossoms and historic temples (not to mention introvert friendly restaurants)</h2>
+      <Calendar />
     </div>
   )
 }
